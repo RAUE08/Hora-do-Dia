eed9c308cd8ce7d3b0e3cc33fcecea676f59c554
RAUE ARAUJO <TIaraujo@outlook.com.br>
2021-01-11 12:49:33 -0300

    Hora-do-Dia-textColor
    Adicionando cores e sequência nas mensagens pelo JavaScript, organizando o codigo para melhores visualizações!

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     var minuto = data.getMinutes()
-    msg.innerHTML = `Agora são exatamente ${hora}:${minuto}`
     if (hora >= 0 && hora < 12){
-        //Bom dia
-        msg.innerHTML += `<p>Bom Dia!</p>`
         img.src = 'fotodia.png'
-        document.body.style.background = '#aed6f8'
-    } else if (hora > 12 && hora <= 18){
-        //Boa tarde
-        msg.innerHTML += `<p>Boa Tarde!</p>`
+            document.body.style.background = '#79a5c1'
+                msg.innerHTML = `<p> <strong>BOM DIA!</strong> </p>`
+                    msg.innerHTML += `<p>Agora são exatamente ${hora}:${minuto} </p>`
+                        msg.style.color = '#79a5c1'
+    } else if (hora >= 12 && hora <= 18){
         img.src = 'fototarde.png'
-        document.body.style.background = '#fbbe6e'
-    } else {
-        //Boa noite
-        msg.innerHTML += `<p>Boa Noite!</p>`
+            document.body.style.background = '#feac3d'
+                msg.innerHTML = `<p> <strong>BOA TARDE!</strong> </p>`
+                    msg.innerHTML += `<p>Agora são exatamente ${hora}:${minuto} </p>`
+                        msg.style.color = '#feac3d'
+    } else{
         img.src = 'fotonoite.png'
-        document.body.style.background = '#061729'
+            document.body.style.background = '#497e9f'
+                msg.innerHTML = `<p> <strong>BOA NOITE!</strong> </p>`
+                    msg.innerHTML += `<p>Agora são exatamente ${hora}:${minuto} </p>`
+                        msg.style.color = '#497e9f'
     }
-}
+    } 
